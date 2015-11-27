0f0cd96247682cba2b9026d388890b016807f3ec
Markus Ahlstrand <markus@ahlstrand.de>
2015-11-27 14:57:22 +0100

    Added config-files and switched audio file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var request = require('request');
 
 app.get("/test.mp3", function(req, res, next) {
-  var audio = request.get('https://acastprod.blob.core.windows.net/media/common/Acast%20logga%2013%20IN%20v2%20320kbpsLevel.mp3');
+  var audio = request.get('http://az592690.vo.msecnd.net/media/channels/varvet/acasts/verket-4-olofwretling-vinterip1/SE/STOCKHOLMSLAN/ALL/varvet-verket-4-olofwretling-vinterip1.mp3?ts=1448627427703');
   audio.on('data', function(chunk) {
     res.write(chunk);
   });
@@ -12,7 +12,8 @@ app.get("/test.mp3", function(req, res, next) {
     res.send();
   });
 
-  res.writeHead(200, {
+    res.writeHead(200, {
+    'Content-Length': 34852688,
     'Content-Type': 'audio/mpeg',
     'Cache-Control': 'public, max-age=86400, s-max-age=86400'
   });
