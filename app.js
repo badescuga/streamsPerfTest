a46f8392bbe85c537a684e7d3380676270d983a8
alex badescu <badescuga@gmail.com>
2015-11-27 16:48:13 +0200

    safe2

--- app.js
+++ app.js
@@ -19,6 +19,40 @@ app.get("/test.mp3", function(req, res, next) {
   });
 });
 
+app.get("/test-cl.mp3", function(req, res, next) {
+  var audio = request.get('http://az592690.vo.msecnd.net/media/channels/varvet/acasts/verket-4-olofwretling-vinterip1/SE/STOCKHOLMSLAN/ALL/varvet-verket-4-olofwretling-vinterip1.mp3?ts=1448627427703');
+  audio.on('data', function(chunk) {
+    res.write(chunk);
+  });
+
+  audio.on('end', function() {
+    res.send();
+  });
+
+    res.writeHead(200, {
+    'Content-Length': 34852688,
+    'Content-Type': 'audio/mpeg',
+    'Cache-Control': 'public, max-age=86400, s-max-age=86400'
+  });
+});
+
+// app.get("/test-local.mp3", function(req, res, next) {
+//   var audio = request.get('http://az592690.vo.msecnd.net/media/channels/varvet/acasts/verket-4-olofwretling-vinterip1/SE/STOCKHOLMSLAN/ALL/varvet-verket-4-olofwretling-vinterip1.mp3?ts=1448627427703');
+//   audio.on('data', function(chunk) {
+//     res.write(chunk);
+//   });
+
+//   audio.on('end', function() {
+//     res.send();
+//   });
+
+//     res.writeHead(200, {
+//     'Content-Length': 34852688,
+//     'Content-Type': 'audio/mpeg',
+//     'Cache-Control': 'public, max-age=86400, s-max-age=86400'
+//   });
+// });
+
 app.set('port', process.env.PORT || 3020);
 
 var server = app.listen(app.get('port'), function() {
