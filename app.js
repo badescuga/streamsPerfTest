6ca81fe50bd412cde128e7109338d82b10ba75ff
alex badescu <badescuga@gmail.com>
2015-12-02 10:58:31 +0200

    safe

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var request = require('request');
 
 app.get("/test.mp3", function (req, res, next) {
   console.log("REQ ----------------------");
-  console.log("REQ PARAMS: " + req.params);
+  console.log("REQ PARAMS: " + JSON.stringify(req.params));
   console.log("REQ PROTOCOL: " + req.protocol);
   console.log("REQ ISFRESH: " + req.fresh);
   console.log("REQ ISSTALE: " + req.stale);
-  console.log("REQ HEADERS: " + req.headers);
+  console.log("REQ HEADERS: " + JSON.stringify(req.headers));
 
   var audio = request.get('http://az592690.vo.msecnd.net/media/channels/varvet/acasts/verket-4-olofwretling-vinterip1/SE/STOCKHOLMSLAN/ALL/varvet-verket-4-olofwretling-vinterip1.mp3?ts=1448627427703');
   audio.on('data', function (chunk) {
