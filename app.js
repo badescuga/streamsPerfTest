89920463e9c894e5a22bc8468ab103d2462bec23
alex badescu <badescuga@gmail.com>
2015-12-02 12:52:36 +0200

    safe

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var request = require('request');
 app.disable('transfer-encoding');
 
 app.get("/test.mp3", function (req, res, next) {
-  console.log("REQ ----------------------");
-  console.log("REQ PARAMS: " + JSON.stringify(req.params));
-  console.log("REQ PROTOCOL: " + req.protocol);
-  console.log("REQ ISFRESH: " + req.fresh);
-  console.log("REQ ISSTALE: " + req.stale);
-  console.log("REQ HEADERS: " + JSON.stringify(req.headers));
+  // console.log("REQ ----------------------");
+  // console.log("REQ PARAMS: " + JSON.stringify(req.params));
+  // console.log("REQ PROTOCOL: " + req.protocol);
+  // console.log("REQ ISFRESH: " + req.fresh);
+  // console.log("REQ ISSTALE: " + req.stale);
+  // console.log("REQ HEADERS: " + JSON.stringify(req.headers));
+
+  console.log("--------- res headers: "+JSON.stringify(res.headers));
 
   var audio = request.get('http://az592690.vo.msecnd.net/media/channels/varvet/acasts/verket-4-olofwretling-vinterip1/SE/STOCKHOLMSLAN/ALL/varvet-verket-4-olofwretling-vinterip1.mp3?ts=1448627427703');
   audio.on('data', function (chunk) {
@@ -20,22 +22,23 @@ app.get("/test.mp3", function (req, res, next) {
     res.send();
   });
 
+
   res.writeHead(200, {
     //    'Content-Length': 34852688,
     'Content-Type': 'audio/mpeg',
     'Test-Header-Badescuga': 'somevalue',
     'Cache-Control': 'public, max-age=86400, s-max-age=86400',
-    'Transfer-Encoding':'blalalba'
+    'Transfer-Encoding':'None'
   });
 });
 
 app.get("/test-cl.mp3", function (req, res, next) {
-   console.log("REQ WITH CL ----------------------");
-  console.log("REQ PARAMS: " + JSON.stringify(req.params));
-  console.log("REQ PROTOCOL: " + req.protocol);
-  console.log("REQ ISFRESH: " + req.fresh);
-  console.log("REQ ISSTALE: " + req.stale);
-  console.log("REQ HEADERS: " + JSON.stringify(req.headers));
+  //  console.log("REQ WITH CL ----------------------");
+  // console.log("REQ PARAMS: " + JSON.stringify(req.params));
+  // console.log("REQ PROTOCOL: " + req.protocol);
+  // console.log("REQ ISFRESH: " + req.fresh);
+  // console.log("REQ ISSTALE: " + req.stale);
+  // console.log("REQ HEADERS: " + JSON.stringify(req.headers));
 
   var audio = request.get('http://az592690.vo.msecnd.net/media/channels/varvet/acasts/verket-4-olofwretling-vinterip1/SE/STOCKHOLMSLAN/ALL/varvet-verket-4-olofwretling-vinterip1.mp3?ts=1448627427703');
   audio.on('data', function (chunk) {
